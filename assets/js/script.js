1136a95659badf3c8e9be7a01b1f91be9d244ec2
inwerk <142039445+inwerk@users.noreply.github.com>
2024-11-10 13:43:23 +0100

    fix: compile TypeScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,6 @@ class LanguagePack {
 class English extends LanguagePack {
     constructor() {
         super("en", {
-            title: "persogenius.de - Generate ID card numbers for German ID cards",
             description: "Generate ID card numbers for German ID cards.",
             header: "ID Card Number Generator",
             authority_id: "Authority ID",
@@ -29,14 +28,13 @@ class English extends LanguagePack {
             reset: "Reset",
             id_card_number: "ID Card Number",
             privacy: "Privacy",
-            privacy_notice: "persogenius.de runs locally in your browser, meaning the server does not store or process any personal data. User inputs remain on the device and are not transmitted to the server."
+            privacy_notice: "PersoGenius runs locally in your browser, meaning the server does not store or process any personal data. User inputs remain on the device and are not transmitted to the server."
         });
     }
 }
 class German extends LanguagePack {
     constructor() {
         super("de", {
-            title: "persogenius.de - Generiere Ausweisnummmern für deutsche Personalausweise",
             description: "Generiere Ausweisnummmern für deutsche Personalausweise.",
             header: "Personalausweisnummer Generator",
             authority_id: "Behördenkennzahl",
@@ -48,14 +46,13 @@ class German extends LanguagePack {
             reset: "Zurücksetzen",
             id_card_number: "Ausweisnummer",
             privacy: "Datenschutz",
-            privacy_notice: "persogenius.de wird lokal in Ihrem Browser ausgeführt, was bedeutet, dass der Server keine personenbezogenen Daten speichert oder verarbeitet. Benutzereingaben bleiben auf dem Gerät und werden nicht an den Server übertragen."
+            privacy_notice: "PersoGenius wird lokal in Ihrem Browser ausgeführt, was bedeutet, dass der Server keine personenbezogenen Daten speichert oder verarbeitet. Benutzereingaben bleiben auf dem Gerät und werden nicht an den Server übertragen."
         });
     }
 }
 function loadLanguage() {
     const language = LanguagePack.load();
     document.documentElement.lang = language.toString();
-    document.title = language.get('title');
     document.head.appendChild(Object.assign(document.createElement('meta'), { name: 'description', content: language.get('description') }));
     const elements = [
         { id: "fieldset-legend", translation: language.get('header') },
